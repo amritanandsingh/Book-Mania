fdc53651d76eefa973dfd705858897356e7e99a5
amritanandsingh <amritanandsingh999@gmail.com>
2023-10-22 22:22:59 +0530

    DB is setted

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555 ; 
+//WmaBIrjs663o7DRS
+
+export const mongoDBURL = "mongodb+srv://amritanandsingh999:WmaBIrjs663o7DRS@cluster0.itefo2b.mongodb.net/books-collection?retryWrites=true&w=majority" ;
