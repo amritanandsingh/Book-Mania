e6632bb3bb61493a179aa9e12d09a18934f529c4
amritanandsingh <amritanandsingh999@gmail.com>
2023-10-24 15:01:15 +0530

    createBook.js is Working by adding app.use(express.json())

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,9 @@ import cors from 'cors' ;
 
 const app = express();
 
-app.use(express.urlencoded());
+//app.use(express.urlencoded());
+app.use(express.json());
+
 app.use(cors());
 
 app.get("/" , (req,res)=>{
